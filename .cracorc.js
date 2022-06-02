6b8bf112482f8ac2e5a89757baf6a576d07b6d19
Oleh Puhalskiy <stergen.pux@gmail.com>
2022-06-02 12:56:39 -0700

    first commit

---
+++ .cracorc.js
@@ -0,0 +1,35 @@
+const { ModuleFederationPlugin } = require("webpack").container;
+
+const deps = require("./package.json").dependencies;
+
+module.exports = () => ({
+  webpack: {
+    configure: {
+      output: {
+        publicPath: "auto",
+      },
+    },
+    plugins: {
+      add: [
+        new ModuleFederationPlugin({
+          name: "father",
+          filename: "remoteEntry.js",
+          remotes: {
+            child: "child@http://localhost:9001/remoteEntry.js",
+          },
+          shared: {
+            ...deps,
+            react: {
+              singleton: true,
+              requiredVersion: deps.react,
+            },
+            "react-dom": {
+              singleton: true,
+              requiredVersion: deps["react-dom"],
+            },
+          },
+        }),
+      ],
+    },
+  },
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
+
+// @ts-ignore
+const Child = React.lazy(() => import('child/App'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense>
+      <div className="App">
+      Father
+      </div>
+      <Child />
+    </Suspense>
   );
 }
 
